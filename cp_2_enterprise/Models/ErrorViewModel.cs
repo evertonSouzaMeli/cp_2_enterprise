64051be69a66d169082ddf8d68348b716340352e
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-10-02 14:50:59 -0300

    feat: start of project

---
+++ cp_2_enterprise/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace cp_2_enterprise.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
